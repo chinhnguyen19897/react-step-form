2d68b7dd45bf2864f70af69ce434bbbe5dcaf99c
TRINITY-HN-006\chinh <chinh.nguyentran@trinity-technology.com>
2023-06-13 14:32:45 +0700

    fix react fragments

--- src/components/form-custom/addons-form/AddonsForm.tsx
+++ src/components/form-custom/addons-form/AddonsForm.tsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
-import { PriceUnit } from "types/form.ts";
-import { StepFormContext } from "@core/context/FormContext.tsx";
-import { CheckboxCard } from "@components/checkbox-card/CheckboxCard";
+import {useContext} from "react";
+import {PriceUnit} from "types/form.ts";
+import {StepFormContext} from "@core/context/FormContext.tsx";
+import {CheckboxCard} from "@components/checkbox-card/CheckboxCard";
 
 export const AddonsForm: React.FC = () => {
   const formAPI = useContext(StepFormContext);
@@ -9,7 +9,7 @@ export const AddonsForm: React.FC = () => {
   if (formAPI?.step != 3) return <></>;
 
   return (
-    <section>
+    <>
       {allAddOns?.map((addOn) => (
         <CheckboxCard
           key={addOn.id}
@@ -30,7 +30,7 @@ export const AddonsForm: React.FC = () => {
           unit={formAPI?.planInfo.pricingType}
         />
       ))}
-    </section>
+    </>
   );
 };
 
--- src/components/form-custom/thank-you-form/ThankYouForm.tsx
+++ src/components/form-custom/thank-you-form/ThankYouForm.tsx
@@ -13,14 +13,16 @@ export const ThankYouForm = () => {
     if(formAPI?.step !== 5) return <></>
 
     return (
-        <FormWrapper>
-            <img src="/images/icon-thank-you.svg" alt="thank-you"/>
-            <h1>Thank You</h1>
-            <p>
-                Thanks for confirming your subscription! We hope you have fun using our
-                platform. If you ever need support, please feel free to email us at
-                support@gaming.com
-            </p>
-        </FormWrapper>
+        <>
+            <FormWrapper>
+                <img src="/images/icon-thank-you.svg" alt="thank-you"/>
+                <h1>Thank You</h1>
+                <p>
+                    Thanks for confirming your subscription! We hope you have fun using our
+                    platform. If you ever need support, please feel free to email us at
+                    support@gaming.com
+                </p>
+            </FormWrapper>
+        </>
     );
 };
