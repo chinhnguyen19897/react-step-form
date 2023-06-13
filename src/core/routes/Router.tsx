f96676beb2b0d07edc40e6adf0b05c010d19cffc
TRINITY-HN-006\chinh <chinh.nguyentran@trinity-technology.com>
2023-06-13 14:37:12 +0700

    fix naming route

--- src/core/routes/Router.tsx
+++ src/core/routes/Router.tsx
@@ -1,4 +1,4 @@
-import routerData from "./routerData.tsx";
+import routerData from "./RouterData.tsx";
 import {routerType} from "./router.types.ts";
 import {Route, Routes} from "react-router-dom";
 import Main from "@layout/Main.tsx";
