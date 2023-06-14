03632d1c241c2fa9aedbe80b3c53ae5fd76ca7c2
TRINITY-HN-006\chinh <chinh.nguyentran@trinity-technology.com>
2023-06-14 15:23:42 +0700

    fix folder structure scss

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { FormSummaryInfo, PersonalInfo } from "types/form.ts";
 
-interface dataPlan {
+interface IDataPlan {
   personalInfoData: PersonalInfo;
   planInfoData: FormSummaryInfo;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useDataContext = () => {
-  const [data, setData] = useState<dataPlan[]>([]);
+  const [data, setData] = useState<IDataPlan[]>([]);
 
   return {
     data,
